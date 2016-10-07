34005706f40aac71b66ee277d6d22b7776cb0ee4
Anant Agarwal <anant@Anants-MacBook-Pro.local>
2016-10-07 14:06:43 -0400

    add calling code in project2driver

--- project2_driver.py
+++ project2_driver.py
@@ -9,8 +9,9 @@ import baseline      #for first baseline
 import baseline1
 import kaggle_op
 import file_reader
+import hmm
 
-local_run = 0;
+local_run = 1;
 
 if( local_run ):
     import proj_config;
@@ -18,15 +19,15 @@ if( local_run ):
     final_path = path+"train/";
     baseline_number =1;
 else:
-        
+
     #
     # User should enter the directory path having 'train' folder.
     #
     input_path_is_correct = 0;
     while (not input_path_is_correct) :
-        path = input("\n\nInput path to the train folder : "); 
+        path = input("\n\nInput path to the train folder : ");
         final_path = path+"train/";
-        print("\nwill start reading at:", final_path, "\n");    
+        print("\nwill start reading at:", final_path, "\n");
         confirm = input("If that's right enter yes else no: "); 
         if (confirm.lower() =="yes") :
             input_path_is_correct = 1
@@ -52,17 +53,17 @@ preprocessor_BIO.preprocess_train_files(path+"train/", path+"train_BIO/")
 '''
 There are 2 Baselines: {1,2} set baseline_number accordingly to run them
 
-'''
+''
 #
 # After the preprocessing generate dictionary of possible weasel words
-#                               
+#
 
 
 
-if(baseline_number == 1):                         
+if(baseline_number == 1):
     run_baseline = baseline;
 elif(baseline_number == 2):
-    run_baseline = baseline1;    
+    run_baseline = baseline1;
 else:
     print("baseline enum is {1,2}\n");
 preprocess_dict = run_baseline.generate_weasel_dictionary(path+"train_BIO/")
@@ -75,7 +76,15 @@ run_baseline.generate_baseline_files(path+"test-public/",
 run_baseline.generate_baseline_files(path+"test-private/",
                                  path+"test-private-baseline1/",
                                  preprocess_dict)
-                                 
+
 kaggle_op.gen_kaggle_file(path+"test-public-baseline1/",
                           path+"test-private-baseline1/",
-                          path) 
+                          path)
+'''
+
+''' sample code for integrating HMM code done till now  is below:'''
+read = hmm.read_everything(path+"train_BIO/");
+#start_tag = hmm.gen_start_tag(3);
+#bio_list = hmm.bio_list_insert_start_tag(read["bio_list"], 2);
+trans_counts = hmm.transition_counts(read["bio_list"],3);
+hmm.display_table(trans_counts["counts_table"]);
